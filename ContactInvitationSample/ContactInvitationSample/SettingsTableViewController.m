0d16bab36f02312dd7a29e0333a6992ba2528b55
Wayne Burkett <wayne.burkett@foresee.com>
2018-08-30 15:53:06 -0400

    MOBILSDK-2166: add settings section for preferred contact type

--- ContactInvitationSample/ContactInvitationSample/SettingsTableViewController.m
+++ ContactInvitationSample/ContactInvitationSample/SettingsTableViewController.m
@@ -10,64 +10,144 @@
 #import "TextFieldTableViewCell.h"
 #import <ForeSee/ForeSee.h>
 
-typedef enum TableSections : NSUInteger {
-    kFSEmailSection = 0,
-    kFSPhoneNumberSection,
-    kFSSectionCount
-} TableSections;
+typedef enum FSTableSections : NSUInteger {
+    EmailEntrySection = 0,
+    PhoneNumberEntrySection,
+    PreferredTypeSelectionSection,
+    SectionCount
+} FSTableSections;
+
+NSString * const FSPreferredContactTypeKey = @"FSPreferredContactTypeKey";
 
 @interface SettingsTableViewController ()
+
+@property (nonatomic, strong) NSUserDefaults *storage;
 
 @end
 
 @implementation SettingsTableViewController
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        self.storage = [NSUserDefaults standardUserDefaults];
+    }
+    return self;
+}
+
+#pragma mark - Preferred type
+
+- (FSContactType)preferredContactType {
+    NSNumber *preferredType = [self.storage objectForKey:FSPreferredContactTypeKey];
+    if (preferredType) {
+        return [preferredType unsignedIntegerValue];
+    }
+    return kFSPhoneNumber;
+}
+
+- (void)setPreferredContactType:(FSContactType)preferredContactType {
+    [ForeSee setPreferredContactType:preferredContactType];
+    [self.storage setInteger:preferredContactType forKey:FSPreferredContactTypeKey];
+    [self.storage synchronize];
+}
+
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return kFSSectionCount;
+    return SectionCount;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     switch (section) {
-        case kFSEmailSection:
+        case EmailEntrySection:
             return @"E-mail";
-        case kFSPhoneNumberSection:
+        case PhoneNumberEntrySection:
             return @"Phone Number";
+        case PreferredTypeSelectionSection:
+            return @"Preferred Contact Type";
     }
     return @"";
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    if (section <= PhoneNumberEntrySection) {
+        return 1;
+    } else if (section == PreferredTypeSelectionSection) {
+        return 2;
+    }
+    return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    TextFieldTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SettingsCell"];
+    NSString *str = @"";
     switch (indexPath.section) {
-        case kFSEmailSection:
-            cell.textField.tag = kFSEmailSection;
-            cell.textField.placeholder = @"example@foresee.com";
-            cell.textField.keyboardType = UIKeyboardTypeEmailAddress;
-            cell.textField.text = [ForeSee contactDetailsForType:kFSEmail];
-            break;
-        case kFSPhoneNumberSection:
-            cell.textField.tag = kFSPhoneNumberSection;
-            cell.textField.placeholder = @"555-555-5555";
-            cell.textField.keyboardType = UIKeyboardTypePhonePad;
-            cell.textField.text = [ForeSee contactDetailsForType:kFSPhoneNumber];
-            break;
+        case EmailEntrySection:
+            return [self createEmailEntryCellForTableView:tableView];
+        case PhoneNumberEntrySection:
+            return [self createPhoneNumberEntryCellForTableView:tableView];
+        case PreferredTypeSelectionSection:
+            switch (indexPath.row) {
+                case kFSEmail:
+                    str = @"E-mail";
+                    break;
+                case kFSPhoneNumber:
+                    str = @"Phone Number";
+                    break;
+                default:
+                    str = @"Unknown";
+                    break;
+            }
+            return [self createSimpleCellForTableView:tableView
+                                                 text:str
+                                             selected:(self.preferredContactType == indexPath.row)];
         default:
             break;
     }
+    return nil; // error
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == PreferredTypeSelectionSection) {
+        [self setPreferredContactType:indexPath.row];
+        [tableView reloadData];
+    }
+}
+
+#pragma mark - Cells
+
+- (UITableViewCell *)createEmailEntryCellForTableView:(UITableView *)tableView {
+    TextFieldTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SettingsCell"];
+    cell.textField.tag = EmailEntrySection;
+    cell.textField.placeholder = @"example@foresee.com";
+    cell.textField.keyboardType = UIKeyboardTypeEmailAddress;
+    cell.textField.text = [ForeSee contactDetailsForType:kFSEmail];
     cell.textField.delegate = self;
+    return cell;
+}
+
+- (UITableViewCell *)createPhoneNumberEntryCellForTableView:(UITableView *)tableView {
+    TextFieldTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SettingsCell"];
+    cell.textField.tag = PhoneNumberEntrySection;
+    cell.textField.placeholder = @"555-555-5555";
+    cell.textField.keyboardType = UIKeyboardTypePhonePad;
+    cell.textField.text = [ForeSee contactDetailsForType:kFSPhoneNumber];
+    cell.textField.delegate = self;
+    return cell;
+}
+
+- (UITableViewCell *)createSimpleCellForTableView:(UITableView *)tableView text:(NSString *)text selected:(BOOL)selected {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"GenericCell"];
+    cell.textLabel.text = text;
+    cell.accessoryType = (selected ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone);
     return cell;
 }
 
 #pragma mark - UITextFieldDelegate
 
 - (void)textFieldDidEndEditing:(UITextField *)textField {
-    [ForeSee setContactDetails:[self trim:textField.text] forType:(textField.tag == kFSEmailSection) ? kFSEmail : kFSPhoneNumber];
+    [ForeSee setContactDetails:[self trim:textField.text] forType:(textField.tag == EmailEntrySection) ? kFSEmail : kFSPhoneNumber];
     [self.tableView reloadData];
 }
 
