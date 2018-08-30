1c543f94b29d9649ed18ce7678e7a635fcdb4dad
Wayne Burkett <wayne.burkett@foresee.com>
2018-08-30 15:16:00 -0400

    MOBILSDK-2166: rename enum identifiers

--- ContactInvitationSample/ContactInvitationSample/SettingsTableViewController.m
+++ ContactInvitationSample/ContactInvitationSample/SettingsTableViewController.m
@@ -10,11 +10,11 @@
 #import "TextFieldTableViewCell.h"
 #import <ForeSee/ForeSee.h>
 
-typedef enum TextFieldRows : NSUInteger {
-    kFSEmailRow = 0,
-    kFSPhoneNumberRow,
-    kFSTextFieldRowCount
-} StyleTypes;
+typedef enum TableSections : NSUInteger {
+    kFSEmailSection = 0,
+    kFSPhoneNumberSection,
+    kFSSectionCount
+} TableSections;
 
 @interface SettingsTableViewController ()
 
@@ -25,14 +25,14 @@ typedef enum TextFieldRows : NSUInteger {
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return kFSTextFieldRowCount;
+    return kFSSectionCount;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     switch (section) {
-        case kFSEmailRow:
+        case kFSEmailSection:
             return @"E-mail";
-        case kFSPhoneNumberRow:
+        case kFSPhoneNumberSection:
             return @"Phone Number";
     }
     return @"";
@@ -45,14 +45,14 @@ typedef enum TextFieldRows : NSUInteger {
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     TextFieldTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SettingsCell"];
     switch (indexPath.section) {
-        case kFSEmailRow:
-            cell.textField.tag = kFSEmailRow;
+        case kFSEmailSection:
+            cell.textField.tag = kFSEmailSection;
             cell.textField.placeholder = @"example@foresee.com";
             cell.textField.keyboardType = UIKeyboardTypeEmailAddress;
             cell.textField.text = [ForeSee contactDetailsForType:kFSEmail];
             break;
-        case kFSPhoneNumberRow:
-            cell.textField.tag = kFSPhoneNumberRow;
+        case kFSPhoneNumberSection:
+            cell.textField.tag = kFSPhoneNumberSection;
             cell.textField.placeholder = @"555-555-5555";
             cell.textField.keyboardType = UIKeyboardTypePhonePad;
             cell.textField.text = [ForeSee contactDetailsForType:kFSPhoneNumber];
@@ -67,7 +67,7 @@ typedef enum TextFieldRows : NSUInteger {
 #pragma mark - UITextFieldDelegate
 
 - (void)textFieldDidEndEditing:(UITextField *)textField {
-    [ForeSee setContactDetails:[self trim:textField.text] forType:(textField.tag == kFSEmailRow) ? kFSEmail : kFSPhoneNumber];
+    [ForeSee setContactDetails:[self trim:textField.text] forType:(textField.tag == kFSEmailSection) ? kFSEmail : kFSPhoneNumber];
     [self.tableView reloadData];
 }
 
