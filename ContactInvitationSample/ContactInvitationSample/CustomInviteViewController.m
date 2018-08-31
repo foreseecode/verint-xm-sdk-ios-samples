69a5f87194aaa4b1cc9468b002f61caeb398c905
Wayne Burkett <wayne.burkett@foresee.com>
2018-08-30 21:40:47 -0400

    MOBILSDK-2166: document the invite lifecycle

--- ContactInvitationSample/ContactInvitationSample/CustomInviteViewController.m
+++ ContactInvitationSample/ContactInvitationSample/CustomInviteViewController.m
@@ -12,55 +12,101 @@
 
 @property (weak, nonatomic) IBOutlet UIButton *inviteAcceptButton;
 @property (weak, nonatomic) IBOutlet UIButton *inviteDeclineButton;
+@property (nonatomic, getter=isShowing) BOOL showing;
 
 @end
 
+/** This class acts as the custom invitation handler. Any class that conforms to <FSInviteHandler> can manage
+ this task. The invite's lifecycle is numerically documented below.
+ 
+ **/
 @implementation CustomInviteViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.inviteAcceptButton.hidden = YES;
-    self.inviteDeclineButton.hidden = YES;
+    // #0 The invite's buttons are hidden to start. They'll be displayed once we determine eligibility.
+    self.inviteAcceptButton.alpha = 0;
+    self.inviteDeclineButton.alpha = 0;
 
+    // #1 We'll start by resetting the SDK's state, so that we can be sure we're eligible to see an invitation. (We'll copy and restore
+    // any saved contact details, so that we still have those, even after a reset.)
+    [self resetAndRestoreContactDetails];
+    
+    // #2 Set this class as the custom invite handler
     [ForeSee setInviteHandler:self];
+    
+    // #3 Check eligibility. If eligible (and we should be), the ForeSee SDK will call the invite handler's `show` method
     [ForeSee checkIfEligibleForSurvey];
 }
 
+#pragma mark - FSInviteHandler
+
+// #4 The `show` method is triggered by the eligibility check. We'll show the invite's accept/decline buttons at this point.
+- (void)show {
+    NSLog(@"showing");
+    [UIView animateWithDuration:0.5
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         self.inviteAcceptButton.alpha = 1;
+                         self.inviteDeclineButton.alpha = 1;
+                     }
+                     completion:^(BOOL finished) {
+                         self.showing = YES;
+                     }];
+}
+
+// #6b The SDK will call this method when the invitation's lifecycle is complete, either because a survey is finished,
+// abandoned, or declined. In any case, we're done!
+- (void)hideWithAnimation:(BOOL)animate {
+    NSLog(@"hiding");
+    self.showing = NO;
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+// #6a The SDK couldn't find contact details for the preferred contact type. The invite is neither accepted or declined.
+- (void)setInvalidInput:(BOOL)isInvalid {
+    NSLog(@"Invalid input");
+    NSLog(@"Current stored email = %@", [ForeSee contactDetailsForType:kFSEmail]);
+    NSLog(@"Current stored phone number = %@", [ForeSee contactDetailsForType:kFSPhoneNumber]);
+}
+
+#pragma mark - Invite actions
+
 - (IBAction)handleAcceptButtonTouchUpInside:(id)sender {
+    // #5a If the accept button is tapped, then we'll notify the ForeSee SDK that the invite was accepted. If the
+    // input is good (i.e. if there is a value stored for the preferred contact type), then the survey is presented. If
+    // the input is bad, then `setInvalidInput:` is called by the SDK.
     [ForeSee customInviteAccepted];
 }
 
 - (IBAction)handleDeclineButtonTouchUpInside:(id)sender {
+    // #5b If the decline button is tapped, then we'll notify the ForeSee SDK that the invite was declined.
     [ForeSee customInviteDeclined];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Reset
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-#pragma mark - FSInviteHandler
-
-- (void)show {
-    NSLog(@"showing");
-    
-    self.inviteAcceptButton.hidden = NO;
-    self.inviteDeclineButton.hidden = NO;
+- (void)resetAndRestoreContactDetails {
+    // A full state-reset also removes the stored contact details. We want to reset the tracker state
+    // while keeping those details. This workaround achieves that. (None if this is necessary in real
+    // implementations. You usually want to maintain state.)
+    NSString *email = [ForeSee contactDetailsForType:kFSEmail];
+    NSString *phoneNumber = [ForeSee contactDetailsForType:kFSPhoneNumber];
+    if (self.isShowing) {
+        // already showing from a previous load; let's reset and start over
+        [ForeSee customInviteDeclined];
+    }
+    [ForeSee resetState];
+    [ForeSee setContactDetails:email forType:kFSEmail];
+    [ForeSee setContactDetails:phoneNumber forType:kFSPhoneNumber];
 }
 
-- (void)hideWithAnimation:(BOOL)animate {
-    NSLog(@"hiding");
-    [self.navigationController popViewControllerAnimated:YES];
-}
-
-- (void)setInvalidInput:(BOOL)isInvalid {
-    NSLog(@"Invalid input");
+- (void)dealloc {
+    if (self.isShowing) {
+        [ForeSee customInviteDeclined];
+    }
 }
 
 @end
