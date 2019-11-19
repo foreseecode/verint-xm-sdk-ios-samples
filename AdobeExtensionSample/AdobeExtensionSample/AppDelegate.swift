e3c7f4ce8d6bb92913cfe9c4373bc5f7fba7dfd4
Wayne Burkett <wayne.burkett@foresee.com>
2019-11-19 14:56:15 -0500

    MOBILSDK-2751: implement ACP

--- AdobeExtensionSample/AdobeExtensionSample/AppDelegate.swift
+++ AdobeExtensionSample/AdobeExtensionSample/AppDelegate.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, ForeSeeAdobeExtensionDelegate {
 
+  let adobeAppId = "launch-ENd52c4e388a484e68955cb76df95d66b6-development"
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    startAdobe()
+    return true
+  }
+  
+  // MARK: Startup
+
+  func startAdobe() {
+    ACPCore.setLogLevel(.debug)
+    ACPCore.configure(withAppId: adobeAppId)
+    
+    ForeSeeAdobeExtension.register()
+    ACPIdentity.registerExtension()
+    ACPLifecycle.registerExtension()
+    ACPSignal.registerExtension()
+    ACPUserProfile.registerExtension()
+    
+    ForeSeeAdobeExtension.setDelegate(self)
+
+    ACPCore.start {
+      ACPCore.lifecycleStart(nil)
+    }
+  }
+  
+  // MARK: ForeSeeAdobeExtensionDelegate
+  
+  func shouldStartSDK() -> Bool {
     return true
   }
 
-
+  func didStartSDK() {
+    print("started ForeSee SDK")
+  }
 }
 
---
+++ AdobeExtensionSample/AdobeExtensionSample/Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  Bridging-Header.h
+//  AdobeExtensionSample
+//
+//  Created by Wayne Burkett on 11/19/19.
+//  Copyright © 2019 ForeSee. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "ACPCore.h"
+#import "ACPIdentity.h"
+#import "ACPLifecycle.h"
+#import "ACPSignal.h"
+#import "ACPUserProfile.h"
+#import "ACPExtensionEvent.h"
+#import "ForeSeeAdobeExtension.h"
+
+#endif /* Bridging_Header_h */
