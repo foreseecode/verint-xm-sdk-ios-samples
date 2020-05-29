91870ec3e9e89b9c39e43b618452f244751894d1
Alan Wang <awangvan@gmail.com>
2020-05-29 09:44:30 -0700

    MOBILSDK-2977: Fixed a local notification bug

--- LocalNotificationSample/LocalNotificationSample/FSViewController.m
+++ LocalNotificationSample/LocalNotificationSample/FSViewController.m
@@ -9,18 +9,31 @@
 #import "FSViewController.h"
 #import <ForeSee/ForeSee.h>
 #import <ForeSeeCxMeasure/ForeSeeCxMeasure.h>
+#import <UserNotifications/UserNotifications.h>
+
 
 @implementation FSViewController
 
 # pragma mark - Actions
 
 - (IBAction)checkEligibility:(id)sender {
+    [self registerNotificationService];
     [ForeSeeCxMeasure checkIfEligibleForSurvey];
 }
 
 - (IBAction)resetState:(id)sender {
     [ForeSee resetState];
 
+}
+
+- (void)registerNotificationService {
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  [center requestAuthorizationWithOptions:(UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert)
+                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    if (!error) {
+      NSLog(@"request authorization succeeded!");
+    }
+  }];
 }
 
 @end
