4c55e74c60be16ec0bd76c62f048cd2f356064a1
Alan Wang <awangvan@gmail.com>
2020-05-29 14:13:13 -0700

    MOBILSDK-2977: Move the requesting notification service call to App Delegate

--- LocalNotificationSample/LocalNotificationSample/FSViewController.m
+++ LocalNotificationSample/LocalNotificationSample/FSViewController.m
@@ -17,23 +17,12 @@
 # pragma mark - Actions
 
 - (IBAction)checkEligibility:(id)sender {
-    [self registerNotificationService];
     [ForeSeeCxMeasure checkIfEligibleForSurvey];
 }
 
 - (IBAction)resetState:(id)sender {
     [ForeSee resetState];
 
-}
-
-- (void)registerNotificationService {
-  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-  [center requestAuthorizationWithOptions:(UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert)
-                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
-    if (!error) {
-      NSLog(@"request authorization succeeded!");
-    }
-  }];
 }
 
 @end
