aa763f4af193d76dfc6445a9d1dc0f80deb889b5
Alan Wang <awangvan@gmail.com>
2020-09-16 10:43:02 -0700

    Add a ForeSeeDelegate demo to the BasicSwiftSample

--- BasicSwiftSample/SwiftSample/AppDelegate.swift
+++ BasicSwiftSample/SwiftSample/AppDelegate.swift
@@ -10,15 +10,29 @@ import UIKit
 import ForeSee
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, ForeSeeDelegate {
   
   var window: UIWindow?
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    ForeSee.start()
+    ForeSee.setDelegate(self)
+    
+    if let path = Bundle.main.path(forResource: "foresee_configuration", ofType: "json") {
+      do {
+        let json = try String(contentsOfFile: path)
+        ForeSee.start(withConfigurationJson: json)
+      } catch {
+        // failed ot load foresee_configuration.json
+      }
+    } else {
+      // foresee_configuration.json file not found
+    }
     return true
   }
   
-  
+  func didStartSDK() {
+    print("ForeSeeDelegate - SDK started")
+  }
+   
 }
 
