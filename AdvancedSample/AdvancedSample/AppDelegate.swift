5098b6a38b36279aa7d9a76a0fed6c8a54814654
Wayne Burkett <wayne.burkett@verint.com>
2020-10-23 17:53:37 -0400

    VOC-13524: use 6.0.2-alpha0

--- AdvancedSample/AdvancedSample/AppDelegate.swift
+++ AdvancedSample/AdvancedSample/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     ForeSee.setDebugLogEnabled(true)
+    ForeSee.setEventLogEnabled(false)
     ForeSee.start()
     ForeSeeCxMeasure.setSkipPoolingCheck(true)
     ForeSee.resetState()
