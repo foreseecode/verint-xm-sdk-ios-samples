7f0603f1f6573f34376ac965bd306c2d2fbcc718
Wayne Burkett <wayne.burkett@verint.com>
2024-04-03 15:20:48 -0400

    Create an inviteless custom invite example

--- AdvancedSample/AdvancedSample/ViewController.swift
+++ AdvancedSample/AdvancedSample/ViewController.swift
@@ -39,6 +39,7 @@ class ViewController: UIViewController {
   @IBAction func checkShowSurveyButtonTouchUpInside(_ sender: Any) {
     let surveyName = NSLocalizedString(Self.surveyNameStringKey, comment: "")
     SurveyManagement.showSurvey(forName: surveyName)
+    SurveyManagement.checkIfEligibleForSurvey()
   }
   
   @IBAction func incrementEventButtonTouchUpInside(_ sender: Any) {
